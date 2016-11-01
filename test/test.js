d45afef404fd3aed567241ace55999d9769c42b0
Petter Ahlberg <petter.ahlberg@tuinordic.com>
2016-11-01 10:53:37 +0100

    Major clean up and some added tests
    The plato report history is now under version control.

--- test/test.js
+++ test/test.js
@@ -3,13 +3,11 @@
 
 "use strict";
 
-var splitFiles = require("../"),
+var splitFiles = require("../index"),
 
-    es = require('event-stream'),
     path = require("path"),
     File = require('vinyl'),
     assert = require("assert"),
-    expect = require("chai").expect,
 
     testUtil = {
         createFakeFile: function (fileName, fileContent) {
@@ -29,20 +27,79 @@ describe("Simple split", function () {
         fileCount = 0,
         fileStream = fileContent.split("/*split*/");
 
-    describe("Should split the file '" + fileName + "' in two", function () {
+    describe("Should split the file '" + fileName + "' in two new files.", function () {
         it("should deliver a file stream containing two files (" + newfileNames.join(" & ") + ")", function (done) {
 
             var fakeFile = testUtil.createFakeFile(fileName, fileContent),
-                split1 = splitFiles();
+                split = splitFiles();
 
-            split1.write(fakeFile);
+            split.write(fakeFile);
 
-            split1.on("data", function (file) {
+            split.on("data", function (file) {
                 var filename = path.basename(file.path);
 
                 assert(file.isBuffer());
                 assert.equal(filename, newfileNames[fileCount]);
                 assert.equal(file.contents.toString('utf8'), fileStream[fileCount]);
+
+                fileCount += 1;
+
+                if (fileCount === fileStream.length) {
+                    done();
+                }
+            });
+        });
+    });
+
+});
+
+describe("Named split", function () {
+
+    var testFile = "testfile2",
+        fileName = testFile + ".txt",
+        fileContent = "/*splitfilename=first.txt*/first/*split*//*splitfilename=second.txt*/second",
+        newFileContent = ["first", "second"],
+        newfileNames = ["first.txt", "second.txt"],
+        fileCount = 0,
+        fileStream = fileContent.split("/*split*/");
+
+    describe("Split '" + fileName + "' into two new named files", function () {
+        it("Should deliver a file stream containing two files (" + newfileNames.join(" & ") + ")", function (done) {
+
+            fileCount = 0;
+
+            var fakeFile = testUtil.createFakeFile(fileName, fileContent),
+                split = splitFiles();
+
+            split.write(fakeFile);
+
+            split.on("data", function (file) {
+                var filename = path.basename(file.path);
+
+                assert(file.isBuffer());
+                assert.equal(filename, newfileNames[fileCount]);
+
+                fileCount += 1;
+
+                if (fileCount === fileStream.length) {
+                    done();
+                }
+            });
+        });
+
+        it("Should remove the splitfilename comment from the new files", function (done) {
+
+            fileCount = 0;
+
+            var fakeFile = testUtil.createFakeFile(fileName, fileContent),
+                split = splitFiles();
+
+            split.write(fakeFile);
+
+            split.on("data", function (file) {
+
+                assert(file.isBuffer());
+                assert.equal(file.contents.toString('utf8'), newFileContent[fileCount]);
 
                 fileCount += 1;
 
